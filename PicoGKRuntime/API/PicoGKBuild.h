219469d9b675edb94118f20dd4f38e32d9c427dc
software tester() <victorjosegarcia888@gmail.com>
2026-09-14 13:15:17 +0200

    Add files via upload

---
+++ PicoGKRuntime/API/PicoGKBuild.h
@@ -0,0 +1,8 @@
+#ifndef PICOGK_BUILD_H
+#define PICOGK_BUILD_H
+
+#define PICOGK_BUILD       "2026-06-05 21:31:47"
+#define PICOGK_LIB_VERSION "26.2.0"
+#define PICOGK_LIB_NAME    "picogk"
+
+#endif // PICOGK_BUILD_H
